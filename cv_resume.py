4a0c56fcfce41a5b8ffe31c54e72cf0b8e6cad0d
naili kamel <naili.kamel@gmail.com>
2019-11-06 23:41:02 +0100

    add parse phone & email from CV : format pdf & docx only supported at the momeent

--- cv_resume.py
+++ cv_resume.py
@@ -1,6 +1,7 @@
 import nltk, os, subprocess, code, glob, re, traceback, sys, inspect
 from time import clock, sleep
 from pprint import pprint
+import textract
 import json
 import zipfile
 # import ner
@@ -8,22 +9,12 @@ import zipfile
 #from convertDocxToText import convertDocxToText
 
 
-# from convertRtfToText import convertRtfToText
-
-
 class exportToCSV:
     def __init__(self, fileName='resultsCSV.txt', resetFile=False):
         headers = ['FILE NAME',
                    'NAME',
-                   'EMAIL1', 'EMAIL2', 'EMAIL3', 'EMAIL4',
-                   'PHONE1', 'PHONE2', 'PHONE3', 'PHONE4',
-                   'INSTITUTES1', 'YEARS1',
-                   'INSTITUTES2', 'YEARS2',
-                   'INSTITUTES3', 'YEARS3',
-                   'INSTITUTES4', 'YEARS4',
-                   'INSTITUTES5', 'YEARS5',
-                   'EXPERIENCE',
-                   'DEGREES',
+                   'EMAIL',
+                   'PHONE'
                    ]
         if not os.path.isfile(fileName) or resetFile:
             # Will create/reset the file as per the evaluation of above condition
@@ -47,31 +38,24 @@ class exportToCSV:
 
             if infoDict['email']:
                 writeString += str(','.join(infoDict['email'][:4])) + ','
-            if len(infoDict['email']) < 4:
-                writeString += ',' * (4 - len(infoDict['email']))
             if infoDict['phone']:
-                writeString += str(','.join(infoDict['phone'][:4])) + ','
-            if len(infoDict['phone']) < 4:
-                writeString += ',' * (4 - len(infoDict['phone']))
-            writeString += str(infoDict['%sinstitute' % 'c\\.?a']) + ","
-            writeString += str(infoDict['%syear' % 'c\\.?a']) + ","
-            writeString += str(infoDict['%sinstitute' % 'b\\.?com']) + ","
-            writeString += str(infoDict['%syear' % 'b\\.?com']) + ","
-            writeString += str(infoDict['%sinstitute' % 'icwa']) + ","
-            writeString += str(infoDict['%syear' % 'icwa']) + ","
-            writeString += str(infoDict['%sinstitute' % 'm\\.?com']) + ","
-            writeString += str(infoDict['%syear' % 'm\\.?com']) + ","
-            writeString += str(infoDict['%sinstitute' % 'mba']) + ","
-            writeString += str(infoDict['%syear' % 'mba']) + ","
-            writeString += str(infoDict['experience']) + ','
-            writeString += str(infoDict['degree']) + '\n'  # For the remaining elements
+                writeString += str(','.join(infoDict['phone'][:4]))
+            writeString += '\n'
             fOut.write(writeString)
         except:
             fOut.write('FAILED_TO_WRITE\n')
         fOut.close()
 
+def read_All_CV(filename):
+    extension = filename.split(".")[-1]
+    if extension == "pdf":
+        text = textract.process(filename, method='pdfminer')
+    elif extension == "docx":
+        tesxt = textract.process(filename, extension='docx')
+    return text.decode('utf-8')
 
-class Parse():
+
+class Parse ():
     # List (of dictionaries) that will store all of the values
     # For processing purposes
     information = []
@@ -82,10 +66,7 @@ class Parse():
 
     def __init__(self, verbose=False):
         print('Starting Programme')
-        fields = ["name", "address", "email", "phone", "mobile", "telephone", "residence status", "experience",
-                  "degree", "cainstitute", "cayear", "caline", "b.cominstitute", "b.comyear", "b.comline",
-                  "icwainstitue", "icwayear", "icwaline", "m.cominstitute", "m.comyear", "m.comline", "mbainstitute",
-                  "mbayear", "mbaline"]
+        fields = ["name", "address", "email", "phone", "mobile", "telephone", "residence status"]
 
         # Glob module matches certain patterns
         doc_files = glob.glob("/tmp/resumes/*.doc")
@@ -103,7 +84,9 @@ class Parse():
             # info is a dictionary that stores all the data obtained from parsing
             info = {}
 
-            self.inputString, info['extension'] = self.readFile(f)
+            #self.inputString = self.readFile(f)
+            self.inputString = read_All_CV(f)
+            info['extension'] = self.inputString
             info['fileName'] = f
 
             self.tokenize(self.inputString)
@@ -114,58 +97,10 @@ class Parse():
 
             self.getName(self.inputString, info)
 
-            self.Qualification(self.inputString, info)
-
-            self.getExperience(self.inputString, info, debug=False)
-
             csv = exportToCSV()
             csv.write(info)
             self.information.append(info)
             print(info)
-
-    def readFile(self, fileName):
-        '''
-        Read a file given its name as a string.
-        Modules required: os
-        UNIX packages required: antiword, ps2ascii
-        '''
-        extension = fileName.split(".")[-1]
-        if extension == "txt":
-            f = open(fileName, 'r')
-            string = f.read()
-            f.close()
-            return string, extension
-        elif extension == "doc":
-            # Run a shell command and store the output as a string
-            # Antiword is used for extracting data out of Word docs. Does not work with docx, pdf etc.
-            return \
-            subprocess.Popen(['antiword', fileName], stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()[
-                0], extension
-        elif extension == "docx":
-            try:
-                return convertDocxToText(fileName), extension
-            except:
-                return ''
-                pass
-        # elif extension == "rtf":
-        #    try:
-        #        return convertRtfToText(fileName), extension
-        #    except:
-        #        return ''
-        #        pass
-        elif extension == "pdf":
-            # ps2ascii converst pdf to ascii text
-            # May have a potential formatting loss for unicode characters
-            # return os.system(("ps2ascii %s") (fileName))
-            try:
-                return convertPDFToText(fileName), extension
-            except:
-                return ''
-                pass
-        else:
-            print
-            'Unsupported format'
-            return '', ''
 
     def preprocess(self, document):
         '''
@@ -202,16 +137,14 @@ class Parse():
             # sentences - split on the basis of rules of grammar
             return tokens, lines, sentences
         except Exception as e:
-            print
-            e
+            print(e)
 
     def tokenize(self, inputString):
         try:
             self.tokens, self.lines, self.sentences = self.preprocess(inputString)
             return self.tokens, self.lines, self.sentences
         except Exception as e:
-            print
-            e
+            print(e)
 
     def getEmail(self, inputString, infoDict, debug=False):
         '''
@@ -226,8 +159,8 @@ class Parse():
             matches = pattern.findall(inputString)  # Gets all email addresses as a list
             email = matches
         except Exception as e:
-            print
-            e
+            print(e)
+
 
         infoDict['email'] = email
 
@@ -305,7 +238,6 @@ class Parse():
         # Lookup in a set is much faster
         indianNames = set(indianNames.split())
 
-        otherNameHits = []
         nameHits = []
         name = None
 
@@ -342,7 +274,7 @@ class Parse():
             if len(nameHits) > 0:
                 nameHits = [re.sub(r'[^a-zA-Z \-]', '', el).strip() for el in nameHits]
                 name = " ".join([el[0].upper() + el[1:].lower() for el in nameHits[0].split() if len(el) > 0])
-                otherNameHits = nameHits[1:]
+
 
         except Exception as e:
             print
@@ -351,140 +283,13 @@ class Parse():
             e
 
         infoDict['name'] = name
-        infoDict['otherNameHits'] = otherNameHits
+
 
         if debug:
             print
             "\n", pprint(infoDict), "\n"
             code.interact(local=locals())
-        return name, otherNameHits
-
-    def getExperience(self, inputString, infoDict, debug=False):
-        experience = []
-        try:
-            for sentence in self.lines:  # find the index of the sentence where the degree is find and then analyse that sentence
-                sen = " ".join([words[0].lower() for words in sentence])  # string of words in sentence
-                if re.search('experience', sen):
-                    sen_tokenised = nltk.word_tokenize(sen)
-                    tagged = nltk.pos_tag(sen_tokenised)
-                    entities = nltk.chunk.ne_chunk(tagged)
-                    for subtree in entities.subtrees():
-                        for leaf in subtree.leaves():
-                            if leaf[1] == 'CD':
-                                experience = leaf[0]
-        except Exception as e:
-            print
-            traceback.format_exc()
-            print
-            e
-        if experience:
-            infoDict['experience'] = experience
-        else:
-            infoDict['experience'] = 0
-        if debug:
-            print
-            "\n", pprint(infoDict), "\n"
-            code.interact(local=locals())
-        return experience
-
-    def getQualification(self, inputString, infoDict, D1, D2):
-        # key=list(qualification.keys())
-        qualification = {'institute': '', 'year': ''}
-        nameofinstitutes = open('nameofinstitutes.txt',
-                                'r').read().lower()  # open file which contains keywords like institutes,university usually  fond in institute names
-        nameofinstitues = set(nameofinstitutes.split())
-        instiregex = r'INSTI: {<DT.>?<NNP.*>+<IN.*>?<NNP.*>?}'
-        chunkParser = nltk.RegexpParser(instiregex)
-
-        try:
-            index = []
-            line = []  # saves all the lines where it finds the word of that education
-            for ind, sentence in enumerate(
-                    self.lines):  # find the index of the sentence where the degree is find and then analyse that sentence
-                sen = " ".join([words[0].lower() for words in sentence])  # string of words
-                if re.search(D1, sen) or re.search(D2, sen):
-                    index.append(ind)  # list of all indexes where word Ca lies
-            if index:  # only finds for Ca rank and CA year if it finds the word Ca in the document
-
-                for indextocheck in index:  # checks all nearby lines where it founds the degree word.ex-'CA'
-                    for i in [indextocheck,
-                              indextocheck + 1]:  # checks the line with the keyword and just the next line to it
-                        try:
-                            try:
-                                wordstr = " ".join(
-                                    words[0] for words in self.lines[i])  # string of that particular line
-                            except:
-                                wordstr = ""
-                            # if re.search(r'\D\d{1,3}\D',wordstr.lower()) and qualification['rank']=='':
-                            # qualification['rank']=re.findall(r'\D\d{1,3}\D',wordstr.lower())
-                            # line.append(wordstr)
-                            if re.search(r'\b[21][09][8901][0-9]', wordstr.lower()) and qualification['year'] == '':
-                                qualification['year'] = re.findall(r'\b[21][09][8901][0-9]', wordstr.lower())
-                                line.append(wordstr)
-                            chunked_line = chunkParser.parse(self.lines[i])  # regex chunk for searching univ name
-                            for subtree in chunked_line.subtrees():
-                                if subtree.label() == 'INSTI':
-                                    for ind, leaves in enumerate(subtree):
-                                        if leaves[0].lower() in nameofinstitutes and leaves[1] == 'NNP' and \
-                                                qualification['institute'] == '':
-                                            qualification['institute'] = ' '.join(
-                                                [words[0] for words in subtree.leaves()])
-                                            line.append(wordstr)
-
-                        except Exception as e:
-                            print
-                            traceback.format_exc()
-
-            if D1 == 'c\.?a':
-                infoDict['%sinstitute' % D1] = "I.C.A.I"
-            else:
-                if qualification['institute']:
-                    infoDict['%sinstitute' % D1] = str(qualification['institute'])
-                else:
-                    infoDict['%sinstitute' % D1] = "NULL"
-            if qualification['year']:
-                infoDict['%syear' % D1] = int(qualification['year'][0])
-            else:
-                infoDict['%syear' % D1] = 0
-            infoDict['%sline' % D1] = list(set(line))
-        except Exception as e:
-            print
-            traceback.format_exc()
-            print
-            e
-
-    def Qualification(self, inputString, infoDict, debug=False):
-        degre = []
-        # Q={'CAinformation':'','ICWAinformation':'','B.Cominformation':'','M.Cominformation':'','MBAinformation':''}
-        # degree=[]
-        # degree1=open('degree.txt','r').read().lower()#string to read from the txt file which contains all the degrees
-        # degree=set(el for el in degree1.split('\n'))#saves all the degrees seperated by new lines,degree name contains both abbreviation and full names check file
-        # qualification1={'CAline':'','CAcollege':'','CArank':'','CAyear':''}
-        self.getQualification(self.inputString, infoDict, 'c\.?a', 'chartered accountant')
-        if infoDict['%sline' % 'c\.?a']:
-            degre.append('ca')
-        self.getQualification(self.inputString, infoDict, 'icwa', 'icwa')
-        if infoDict['%sline' % 'icwa']:
-            degre.append('icwa')
-        self.getQualification(self.inputString, infoDict, 'b\.?com', 'bachelor of commerce')
-        if infoDict['%sline' % 'b\.?com']:
-            degre.append('b.com')
-        self.getQualification(self.inputString, infoDict, 'm\.?com', 'masters of commerce')
-        if infoDict['%sline' % 'm\.?com']:
-            degre.append('m.com')
-        self.getQualification(self.inputString, infoDict, 'mba', 'mba')
-        if infoDict['%sline' % 'mba']:
-            degre.append('mba')
-        if degre:
-            infoDict['degree'] = degre
-        else:
-            infoDict['degree'] = "NONE"
-        if debug:
-            print
-            "\n", pprint(infoDict), "\n"
-            code.interact(local=locals())
-        return infoDict['degree']
-
+        return name
 
 if __name__ == "__main__":
     verbose = False
